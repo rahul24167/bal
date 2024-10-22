2e6e38c57b82121a1bf8ee035da2dffd231d9444
rahul24167 <113711057+rahul24167@users.noreply.github.com>
2024-10-23 01:20:10 +0530

    error in dashbord.ts due to middleware has been resolved

--- backend/src/routes/dashboard.ts
+++ backend/src/routes/dashboard.ts
@@ -1,16 +1,9 @@
 import express, { Request, Response, NextFunction} from 'express';
 const router = express.Router();
 import authMiddleware from '../middleware';
-interface CustomRequest extends Request {
-    headers: {
-      authorization: string;
-    },
-    userId?: string;
-}
-router.post("/",authMiddleware, async (req:Request, res: Response, next: NextFunction):Promise<any> => {
-    return res.status(200).json({
-        message: "days  and session changed successfully"
-    })
+
+router.post("/*",authMiddleware, async (req, res):Promise<any> => {
+    return res.status(200).json({success: true})
 });
 
 
