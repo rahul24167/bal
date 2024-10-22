ec7c21746ce903ac6b1895ad4b643f7800ee9b6a
Om Wadhi <omwadhi64@gmail.com>
2024-10-23 00:56:21 +0530

    Update middleware.ts

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -1,33 +1,36 @@
+import { NextFunction, Request, Response } from "express";
 import * as jwt from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
+
 const JWT_SECRET = process.env.JWT_SECRET;
 
-interface CustomRequest extends Request {
-    headers: {
-      authorization: string;
-    },
-    userId?: string;
-}interface JwtPayloadWithUserId extends jwt.JwtPayload {
-    userId: string;
+interface JwtPayloadWithUserId {
+  userId: string;
+}
+export const authMiddleware = (req : Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.status(403).json({});
+    return;
   }
-const authMiddleware =(req:Request,res:Response,next:NextFunction) => {
-    const authHeader = req.headers.authorization;
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-        return res.status(403).json({});
+  const token = authHeader.split(" ")[1];
+  console.log(token);
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET ?? "");
+    const { userId } = decoded as JwtPayloadWithUserId;
+    if (userId) {
+      req.body.userId = userId;
+      next();
+      return;
+    } else {
+       res.status(403).json({});
+       return;
     }
-    const token = authHeader.split(' ')[1];
-    try{
-        const decoded = jwt.verify(token, JWT_SECRET as string) as JwtPayloadWithUserId;
-        if(decoded.userId){
-            req.body.userId = decoded.userId;
-            next();
-        }else{
-            return res.status(403).json({});
-        }
-        
-    }catch(err){
-        return res.status(403).json({});
-    }
+  } catch (err) {
+     res.status(403).json({});
+     return;
+  }
 };
 
-export default authMiddleware;
+
+
