8588543bad836d4296f7f62f703bb7df2cd5c476
rahul24167 <113711057+rahul24167@users.noreply.github.com>
2024-10-30 12:26:44 +0530

    just started with the signin /signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
+import { BrowserRouter } from "react-router-dom";
+import { LandingPage } from "./components/LandingPage"
+import { LoginForm } from "./components/login-form"
+import { SignupForm } from "./components/signup-form";
+import { Navbar } from "./components/navbar";
+ 
 function App() {
   return (
     <>
+    {/* <LandingPage/> */}
+    <Navbar></Navbar>
+
+    <BrowserRouter>
+    {/* <Routes>
+      <Route path=""/>
+    </Routes> */}
+    <SignupForm/>
+    <LoginForm/>
+    </BrowserRouter>
     </>
   )
 }
+
 
 export default App
